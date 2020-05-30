142476faef04999c20105e1c03ddfcad23b30949
andrewtdiz <andrewdizenzojhu@gmail.com>
2020-05-30 14:38:49 -0400

    Template

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <p class="text-5xl text-red-500">Hi</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
